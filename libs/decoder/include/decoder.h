47d31a00efc3328d40b96c4c7207430dd1da18a5
nvt <trangnguyenvan013@gmail.com>
2026-09-03 16:45:53 +0700

    update something

--- libs/decoder/include/decoder.h
+++ libs/decoder/include/decoder.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #ifndef ATHENA_CORE
 #define ATHENA_CORE
 
@@ -10,6 +11,7 @@
 
 namespace athena::decoder {
 enum athena_status { ATHENA_ERROR_CODE = 0, ATHENA_SUCCESS_CODE = 1 };
+enum athena_mode { OFFLINE_MODE, LIVE_MODE };
 
 /* @brief Baseband typy
  */
@@ -26,44 +28,69 @@ enum baseband_t {
  */
 enum until_t { SOFT, CADU, PRODUCTS };
 
-/* @brief Command structure
- */
-class cmd {
+class result {
   public:
-    /* @brief Pipelines support list: meteor_m2-x_lrpt, gk2a_lrit.
-     * @params string pipeline (mandatory): meteor_m2-x_lrpt or gk2a_lrit.
-     * @params string input_file (mandatory): input baseband file.
-     * @params string output_directory (mandatory): output folder.
-     * @params until_t until (optional): default PRODUCTS level.
-     * @params u64 samplerate:
-     *      - Mandatory: .cf32, .cs32, .cs16, .cs8, .cu8
-     *      - Optional: .wav
-     * @params baseband_t baseband_format (mandatory): baseband type, default
-     * cf32.
-     * @params bool iq_swap:
-     *      - True: I/Q phase swap.
-     * @params buffer_size (optional): override default buffer size - default =
-     * 8192 bytes.
-     * @params bool live (optional):
-     *      - True: Enable live mode.
-     *      - False: Offline mode.
-     */
-    std::string pipeline = "";
-    std::string input_file = "";
-    std::string output_directory = "";
-    until_t until = PRODUCTS;
-    uint64_t samplerate = 0;
-    baseband_t baseband_format = CF_32;
-    bool iq_swap{false};
-    uint32_t buffer_size = 0;
-    bool live{false};
+    std::shared_ptr<std::vector<data::meteor::meteor_telemetry>> telemetry;
+    athena_status status;
 };
+
+class listener {
+  public:
+    class result result;
+
+  public:
+    virtual void on_progress(double progress) = 0;
+    virtual void on_finished() = 0;
+    virtual void on_error(std::string error) = 0;
+};
+
+/* @brief Call this function to initialize decoder system
+ */
+athena_status init_decoder();
 
 /* @brief Call this function to demodulate and decode baseband file
  */
-athena_status
-decode(cmd cmd,
-       std::shared_ptr<std::vector<data::meteor::meteor_telemetry>> *telemetry);
+void decode(std::shared_ptr<listener> listener);
+
+/* @brief Call this function to set decoder mode
+ */
+void set_mode(athena_mode mode);
+
+/* @brief Call this function to set pipeline
+ */
+athena_status set_pipeline(std::string pipeline_id);
+
+/* @brief Call this function to set input_file
+ */
+void set_input_file(std::string input_file);
+
+/* @brief Call this function to set output_directory
+ */
+void set_output_directory(std::string output_directory);
+
+/* @brief Call this function to set until
+ */
+athena_status set_until(until_t until);
+
+/* @brief Call this function to set samplerate
+ */
+void set_samplerate(uint64_t samplerate);
+
+/* @brief Call this function to set baseband format
+ */
+void set_baseband_format(baseband_t baseband_format);
+
+/* @brief Call this function to set iq_swap
+ */
+void set_iq_swap(bool iq_swap);
+
+/* @brief Call this function to set buffer size
+ */
+void set_buffer_size(uint32_t buffer_size);
+
+/* @brief Call this function to enable doppler logging
+ */
+void enable_doppler_logging();
 
 } // namespace athena::decoder
 
--- main.cpp
+++ main.cpp
@@ -2,62 +2,88 @@
 #include <stdio.h>
 #include <vector>
 
-int main() {
-    athena::decoder::cmd cmd;
-    cmd.pipeline = "meteor_m2-x_lrpt";
-    cmd.input_file = "/home/nvt/Documents/Satdump-input/test.wav";
-    // cmd.input_file = "/home/nvt/Documents/Satdump-input/"
-    //                  "2026-08-17_07-28-50_1024000SPS_137900000Hz.cf32";
-    cmd.output_directory = "outputtest";
-    cmd.baseband_format = athena::decoder::CF_32;
-    cmd.samplerate = 1024e3;
-    cmd.live = true;
-
-    std::shared_ptr<std::vector<athena::data::meteor::meteor_telemetry>>
-        telemetry = std::make_shared<
-            std::vector<athena::data::meteor::meteor_telemetry>>();
-
-    athena::decoder::athena_status success =
-        athena::decoder::decode(cmd, &telemetry);
-
-    printf("size of telemetry = %d\n", (int)telemetry->size());
-
-    athena::data::meteor::meteor_telemetry el = (*telemetry)[0];
-
-    int cnt = 0;
-    while (cnt < 10) {
-        el = (*telemetry)[cnt++];
-        if (el.type == athena::data::meteor::NONE) {
-            printf("{\n");
-            printf("    APID = %s\n", el.APID);
-            printf("    timestamp = %lf\n", el.ccsds_time);
-            printf("    {\n");
-            printf("        msu_mr_id = %s\n", el.msu_mr_id);
-            printf("        msu_mr_set = %s\n", el.msu_mr_set);
-            printf("    }\n");
-            printf("},\n");
-        } else {
-            printf("{\n");
-            printf("    APID = %s\n", el.APID);
-            printf("    timestamp = %lf\n", el.ccsds_time);
-            printf("    {\n");
-            printf("        msu_mr_id = %s\n", el.msu_mr_id);
-            printf("        msu_mr_set = %s\n", el.msu_mr_set);
-            printf("    }\n");
-            printf("    {\n");
-            printf("    %s: {\n", el.type_name);
-            for (int i = 0; i < 26; ++i)
-                printf("        %s = %s\n", el.telemetry[0][i],
-                       el.telemetry[1][i]);
-            printf("    }\n");
-            printf("},\n");
+class m_listener : public athena::decoder::listener {
+  public:
+    void on_progress(double progress) {
+        printf("----> progress %lf\n", progress);
+    }
+    void on_finished() {
+        if (result.status == athena::decoder::ATHENA_ERROR_CODE) {
+            printf("Decoding Error");
+            return;
         }
-    }
 
-    if (success == athena::decoder::ATHENA_ERROR_CODE) {
-        printf("Decoding Error");
-        return 0;
+        printf("size of telemetry = %d\n", (int)result.telemetry->size());
+
+        athena::data::meteor::meteor_telemetry el = (*result.telemetry)[0];
+
+        int cnt = 0;
+        while (cnt < 10) {
+            el = (*result.telemetry)[cnt++];
+            if (el.type == athena::data::meteor::NONE) {
+                printf("{\n");
+                printf("    APID = %s\n", el.APID);
+                printf("    timestamp = %lf\n", el.ccsds_time);
+                printf("    {\n");
+                printf("        msu_mr_id = %s\n", el.msu_mr_id);
+                printf("        msu_mr_set = %s\n", el.msu_mr_set);
+                printf("    }\n");
+                printf("},\n");
+            } else {
+                printf("{\n");
+                printf("    APID = %s\n", el.APID);
+                printf("    timestamp = %lf\n", el.ccsds_time);
+                printf("    {\n");
+                printf("        msu_mr_id = %s\n", el.msu_mr_id);
+                printf("        msu_mr_set = %s\n", el.msu_mr_set);
+                printf("    }\n");
+                printf("    {\n");
+                printf("    %s: {\n", el.type_name);
+                for (int i = 0; i < el.type; ++i)
+                    printf("        %s = %s\n", el.telemetry[0][i],
+                           el.telemetry[1][i]);
+                printf("    }\n");
+                printf("},\n");
+            }
+        }
+
+        printf("Decoding finished");
+
+        printf("on finished!\n");
     }
-    printf("Successfully Decoded");
+    void on_error(std::string error) { printf("error: %s\n", error.c_str()); }
+};
+
+int main() {
+    // athena::decoder::cmd cmd;
+    // cmd.pipeline = "meteor_m2-x_lrpt";
+    // cmd.input_file = "/home/nvt/Documents/Satdump-input/test2.wav";
+    // // cmd.input_file = "/home/nvt/Documents/Satdump-input/"
+    // //                  "2026-08-17_07-28-50_1024000SPS_137900000Hz.cf32";
+    // cmd.output_directory = "outputtest2";
+    // cmd.baseband_format = athena::decoder::CF_32;
+    // cmd.samplerate = 1024e3;
+    // cmd.live = true;
+
+    athena::decoder::init_decoder();
+
+    athena::decoder::set_pipeline("meteor_m2-x_lrpt");
+    athena::decoder::set_mode(athena::decoder::LIVE_MODE);
+    // athena::decoder::set_input_file(
+    // "/home/nvt/Documents/Satdump-input/test.wav");
+    athena::decoder::set_input_file(
+        "/home/nvt/Documents/Satdump-input/"
+        "2026-08-17_07-28-50_1024000SPS_137900000Hz.cf32");
+    athena::decoder::set_output_directory("outputtest3");
+    athena::decoder::set_baseband_format(athena::decoder::CF_32);
+    athena::decoder::set_samplerate(1024e3);
+    athena::decoder::enable_doppler_logging();
+
+    std::shared_ptr<athena::decoder::listener> listener =
+        std::make_shared<m_listener>();
+    listener->result.telemetry =
+        std::make_shared<std::vector<athena::data::meteor::meteor_telemetry>>();
+
+    athena::decoder::decode(listener);
     return 0;
 }
